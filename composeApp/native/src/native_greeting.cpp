01ab2a0d0788ef26849415afa40122d3de011d53
muazkadan <muaz.kadan@gmail.com>
2025-06-28 08:17:20 +0300

    Remove main function from native_greeting.cpp

--- composeApp/native/src/native_greeting.cpp
+++ composeApp/native/src/native_greeting.cpp
@@ -1,14 +1,8 @@
 #include <cstring>
-#include <iostream>
 
 extern "C" {
     const char* getNativeGreeting() {
         static const char* greeting = "Hello from C++";
         return greeting;
     }
-}
-
-int main() {
-    std::cout << getNativeGreeting() << std::endl;
-    return 0;
 }
